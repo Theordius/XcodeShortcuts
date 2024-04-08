d08aafbef43e5d7e9e9b8a81240dac7af4fb0735
Theordius <87307483+Theordius@users.noreply.github.com>
2024-04-08 12:02:01 +0200

    Initial Commit

---
+++ XcodeShortcuts/XcodeShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeShortcutsApp.swift
+//  XcodeShortcuts
+//
+//  Created by Rafał Gęsior on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
